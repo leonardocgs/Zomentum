543b99292b729b957070af61b270af2fdc6b2da9
Leonardo Cesar <leonardocesargoes@gmail.com>
2022-11-14 19:14:29 -0300

    feat: add nav menu

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,12 @@
 import HamburguerMenuIcon from "./HamburgerMenuIcon";
+import Navigation from "./Navigation";
 
 const Header = () => {
   return (
     <header className="header">
       <img src="/zomentum_logo.png" alt="Logo da Zumentum" />
       <HamburguerMenuIcon />
+      <Navigation />
     </header>
   );
 };
---
+++ src/Navigation.jsx
@@ -0,0 +1,21 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Features</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+        <li>
+          <a href="#">Get a Demo</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navigation;
