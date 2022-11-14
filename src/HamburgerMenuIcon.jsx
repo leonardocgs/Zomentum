8025cc24a21cc7707957c8428c15b7b5f3597517
Leonardo Cesar <leonardocesargoes@gmail.com>
2022-11-14 18:22:47 -0300

    fix: hamburguer menu icon

--- src/HamburgerMenuIcon.jsx
+++ src/HamburgerMenuIcon.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 const HamburguerMenuIcon = () => {
+  const [isActive, setActive] = useState(false);
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
+  const parentClassName = "hamburguer-menu-icon";
   return (
-    <div className="hamburguer-menu-icon">
+    <button
+      onClick={handleToggle}
+      className={isActive ? parentClassName + " clicked" : parentClassName}
+    >
       <div className="hamburguer-menu-bar"></div>
       <div className="hamburguer-menu-bar"></div>
       <div className="hamburguer-menu-bar"></div>
-    </div>
+    </button>
   );
 };
 export default HamburguerMenuIcon;
