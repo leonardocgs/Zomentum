41520838484d0cf6136cbdc8074421a182b360cc
Leonardo Cesar <leonardocesargoes@gmail.com>
2022-11-15 02:06:40 -0300

    feat: add Features component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
 import Header from "./Header";
 import "./App.css";
 import Hero from "./Hero";
+import Features from "./Features";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Features />
     </div>
   );
 }
---
+++ src/Features.jsx
@@ -0,0 +1,25 @@
+const Features = () => {
+  return (
+    <section className="features-section">
+      <h2>List. Sell. Grow</h2>
+      <div className="features-container">
+        <div className="container">
+          <img
+            src="/feat-1.svg"
+            alt="A icon that represents a computer's windows"
+          />
+          <p>Be a part of the only All-In-One platform for IT </p>
+        </div>
+        <div className="container">
+          <img src="/feat-2.svg" alt="A icon that represents a magnifier" />
+          <p>Come closer to finding your next customer</p>
+        </div>
+        <div className="container">
+          <img src="/feat-3.svg" alt="A icon that represents math graphics" />
+          <p>Grow your business with a single click</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Features;
