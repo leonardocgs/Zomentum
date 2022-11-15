6915d272dca2cabcbbc4357d8ef604092d3b8fe3
Leonardo Cesar <leonardocesargoes@gmail.com>
2022-11-14 23:23:07 -0300

    feat: add Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Header from "./Header";
 import "./App.css";
+import Hero from "./Hero";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      <div className="container">
+        <h1>
+          Find the right partners to <hr />
+          fuel your business growth
+        </h1>
+        <p>
+          Join a vibrant community of MSPs to forge long-lasting relationships
+          with partners that help you create excellent customer experiences
+        </p>
+        <button> Register today</button>
+      </div>
+    </section>
+  );
+};
+export default Hero;
