a06cd86aa0d833a1c2cdcdf9e347385f4bb8dc9f
Leonardo Cesar <leonardocesargoes@gmail.com>
2022-11-14 23:38:55 -0300

    fix: nav hamburguer menu

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,10 +2,7 @@ const Hero = () => {
   return (
     <section className="hero-section">
       <div className="container">
-        <h1>
-          Find the right partners to <hr />
-          fuel your business growth
-        </h1>
+        <h1>Find the right partners to fuel your business growth</h1>
         <p>
           Join a vibrant community of MSPs to forge long-lasting relationships
           with partners that help you create excellent customer experiences
